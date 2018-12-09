7ffbba903e4c62b4b8e807dbec88b4b6b2caaa73
Ewa Lorecka <lorecka.ewa@gmail.com>
2018-12-09 11:25:01 +0000

    add bind function

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
         otherState: "some other value"
     }
     
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
         //console.log("Was clicked!");
         // Don´t do this this.state.persons[0].name = "Ula";
         this.setState({
             persons:[
-                { name: "Ewelina", age: "18"},
+                { name: newName, age: "18"},
                 { name: "Udo", age: "25"},
                 { name: "Bartosz", age: "55"},
                 { name: "Miro", age: "20"}
@@ -32,14 +32,14 @@ class App extends Component {
       <div className="App">
        <h1>Hi, I am a React App</h1>
        <p>This is really working!</p>
-       <button onClick = {this.switchNameHandler} >Switch Name</button>
+       <button onClick = {this.switchNameHandler.bind(this, "Ewelina")} >Switch Name</button>
        <Person 
          name = {this.state.persons[0].name} 
          age = {this.state.persons[0].age}/>
        <Person 
          name = {this.state.persons[1].name} 
          age = {this.state.persons[1].age}
-         click={this.switchNameHandler} >My Hobbies: Real estate</Person>
+         click={this.switchNameHandler.bind(this, "Ewe!")} >My Hobbies: Real estate</Person>
        <Person 
          name = {this.state.persons[2].name} 
          age = {this.state.persons[2].age}/>
