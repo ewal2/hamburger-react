c89b7793c63a89b50bfed879db7c8b28f602449b
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-06 19:46:35 +0000

    add componentDidUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -16,6 +16,10 @@ class Persons extends Component {
          console.log('[Persons.js] getSnapshotBeforeUpdate');
        }
        
+       componentDidUpdate() {
+         console.log('[Persons.js] componentDidUpdate');
+       }
+       
         render() {
           console.log('[Persons.js] rendering...');
           return this.props.persons.map((person, index) => {
