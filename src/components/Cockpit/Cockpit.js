26e17580fb409bc218b9313ced9f26e79a71e379
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-07 15:50:22 +0000

    add return to const cockpit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,6 +8,9 @@ const cockpit = ( props ) => {
         setTimeout(() => {
             alert('Saved data to could!');
         }, 1000);
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect');
+        }
     }, []);
     
     const assignedClasses = [];
