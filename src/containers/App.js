77979188253701372b60b1d57520bb5ec26ce5a1
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-07 12:25:35 +0000

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,6 +36,16 @@ class App extends Component {
         console.log('[App.js] componentDidMount');
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[App.js] shouldComponentUpdate');
+    }
+
+    componentDidUpdate() {
+        console.log('[App.js] componentDidUpdate');
+    }
+    
+    
+    
     nameChangedHandler = ( event, id ) => {
         const personIndex = this.state.persons.findIndex(p => {
             return p.id === id;
