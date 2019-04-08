8b33d82e3443633fa7b29b97e105b3bbbb3fb343
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-08 15:31:00 +0000

    update

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -50,12 +50,12 @@ class Persons extends PureComponent {
           console.log('[Persons.js] rendering...');
           return this.props.persons.map((person, index) => {
           return <Person 
-            click={() => this.props.clicked(index)}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.props.changed(event, person.id)} 
-            />;
+                    click={() => this.props.clicked(index)}
+                    name={person.name} 
+                    age={person.age}
+                    key={person.id}
+                    changed={(event) => this.props.changed(event, person.id)} 
+                 />;
           });
         }
       }
