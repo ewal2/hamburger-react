e07d763252263a05b20416d8e4b92ac6de0e9c9b
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-16 17:45:30 +0000

    remove commas

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,9 +11,13 @@ class Person extends Component {
       console.log('[Person.js] rendering...');
       return (
       <Aux>
-        <p key="i1" onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!</p>,
-        <p key="i2">{this.props.children}</p>,
-        <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}
+        <p key="i1" onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!</p>
+        <p key="i2">{this.props.children}</p>
+        <input 
+          key="i3" 
+          type="text"
+          onChange={this.props.changed} 
+          value={this.props.name}
         />
       </Aux>
     );
