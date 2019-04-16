f14dcfaff55728db63630b0c1f86f691f4d993a2
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-16 18:10:46 +0000

    update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,16 @@ import classes from "./Person.css";
 
 
 class Person extends Component {
+    constructor(props) {
+      super(props);
+      this.inputElementRef = React.createRef();
+    }
+  
+    componentDidMount() {
+      //this.inputElement.focus();
+      this.inputElementRef.current.focus();
+    }
+    
     render() {
       console.log('[Person.js] rendering...');
       return (
@@ -15,6 +25,8 @@ class Person extends Component {
         <p key="i2">{this.props.children}</p>
         <input 
           key="i3" 
+          //ref={(inputEl) => {this.inputElement = inputEl}}
+          ref = {this.inputElementRef}
           type="text"
           onChange={this.props.changed} 
           value={this.props.name}
