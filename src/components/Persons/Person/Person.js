e70b075c2018d042829a241396c53a5ffcfda4ba
Ewa Lorecka <lorecka.ewa@gmail.com>
2019-04-16 19:13:47 +0000

    update

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -20,18 +20,19 @@ class Person extends Component {
     render() {
       console.log('[Person.js] rendering...');
       return (
-      <Aux>
-        <p key="i1" onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!</p>
-        <p key="i2">{this.props.children}</p>
-        <input 
-          key="i3" 
-          //ref={(inputEl) => {this.inputElement = inputEl}}
-          ref = {this.inputElementRef}
-          type="text"
-          onChange={this.props.changed} 
-          value={this.props.name}
-        />
-      </Aux>
+        <Aux>
+          {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
+          <p key="i1" onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!</p>
+          <p key="i2">{this.props.children}</p>
+          <input 
+            key="i3" 
+            //ref={(inputEl) => {this.inputElement = inputEl}}
+            ref = {this.inputElementRef}
+            type="text"
+            onChange={this.props.changed} 
+            value={this.props.name}
+          />
+        </Aux>
     );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -23,7 +23,9 @@ class App extends Component {
         otherState: "some other value",
         showPersons: false,
         showCockpit: true,
-        changeCounter: 0
+        changeCounter: 0,
+        authenticated: false
+        
     }
     
     static getDerivedStateFromProps(props, state) {
@@ -86,21 +88,25 @@ class App extends Component {
     }
     
    
+    loginHandler = () => {
+        this.setState({authenticated: true});
+    }
     
-  render() {
-    console.log('[App.js] render');
-      
-    let persons = null;
-
-    if ( this.state.showPersons ) {
-        persons = (
-            <Persons 
-                 persons={this.state.persons}
-                 clicked={this.deletePersonHandler}
-                 changed={this.nameChangedHandler}
-            />
-        )
-    };
+    render() {
+        console.log('[App.js] render');
+          
+        let persons = null;
+    
+        if ( this.state.showPersons ) {
+            persons = (
+                <Persons 
+                     persons={this.state.persons}
+                     clicked={this.deletePersonHandler}
+                     changed={this.nameChangedHandler}
+                     isAuthenticated={this.state.authenticated}
+                />
+            )
+     };
     
     
       
@@ -110,12 +116,13 @@ class App extends Component {
               }}
               >Remove Cockpit</button>
               { this.state.showCockpit ? (
-              <Cockpit
-                title={this.props.appTitle}
-                showPersons={this.state.showPersons}
-                personsLength={this.state.persons.length}
-                clicked={this.togglePersonsHandler} 
-                /> 
+                  <Cockpit
+                    title={this.props.appTitle}
+                    showPersons={this.state.showPersons}
+                    personsLength={this.state.persons.length}
+                    clicked={this.togglePersonsHandler} 
+                    login={this.loginHandler}
+                  /> 
                 ) : null}
               {persons}
           </Aux>
